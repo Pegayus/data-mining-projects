4d46f0f2aeb860c66fada51c04eff6bdc580a0a4
Pegah Hozhabrierdi <33411906+Pegayus@users.noreply.github.com>
2019-01-08 00:47:23 -0500

    Update bloom_filter_mmh3.py

--- bloom_filter/bloom_filter_mmh3.py
+++ bloom_filter/bloom_filter_mmh3.py
@@ -1,13 +1,4 @@
-
-# coding: utf-8
-
-# In[6]:
-
-
 import mmh3
-
-# In[14]:
-
 
 # Define hash functions
 #h = [blake2b(), md5(), sha1(), sha224(), sha256(), sha384(), sha3_224(), sha3_256(), sha3_384(), sha3_512()]
@@ -16,10 +7,6 @@ import mmh3
 #B_dec = int(B_bin, 2)
 B_dec = 0
 n = 6124450
-
-
-# In[15]:
-
 
 #reading files
 path1=r'C:\Users\pegah\Desktop\univ\Courses\data mining\HW5\P2\listed_username_30.txt'
@@ -31,19 +18,12 @@ with open(path2,encoding="utf8") as f:
     content365 = f.readlines()
 content365 = [x.strip('\n') for x in content365]
 
-
-# In[19]:
-
 k=10
 # Filling B with S (content30)
 for i in range(len(content30)):
     for j in range(k):
         hash_dec = mmh3.hash(content30[i],j, signed=False)%(2^n)
         B_dec = hash_dec | B_dec
-
-
-# In[36]:
-
 
 # Checing spams from content365
 spam = []
@@ -65,17 +45,11 @@ for i in range(len(content365)):
 #print(len(spam))
 #print(type(spam))
 #print(spam[:10])
-# In[26]:
 
 # False-positive rate
 false_positive = len(set(spam)) - len(list(set(content30).intersection(spam)))
 true_negative = len(content365) - len(spam)
 false_positive_rate = false_positive / (true_negative + false_positive)
 
-
-# In[ ]:
-
-
 print('optimal k for n = {0} is {1}'.format(n, k))
 print('false positive rate (false postive/(false positive + true negative)) is {0:0.4f}'.format(false_positive_rate))
-
