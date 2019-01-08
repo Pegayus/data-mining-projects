254bc1274ed1eabf64657b356c95ca63fc3f65d5
Pegah Hozhabrierdi <33411906+Pegayus@users.noreply.github.com>
2019-01-08 00:39:53 -0500

    Update text_clustering.py

--- text_clustering/src/text_clustering.py
+++ text_clustering/src/text_clustering.py
@@ -1,11 +1,8 @@
-
-# coding: utf-8
-
-# In[91]:
-
-#read data
 import re
 import numpy as np
+import collections
+
+#read data
 path=r'C:\Users\pegah\Desktop\univ\courses\data mining\HW3\foods.txt'
 with open(path) as f:
     text=f.readlines()
@@ -15,17 +12,11 @@ with open(path2) as f:
     stops=f.readlines()
 stops=[x.strip() for x in stops]
 
-
-# In[92]:
-
 #extract reviews
 review=[]
 for i in range(len(text)):
     if text[i][0:13]=='review/text: ':
         review.append(text[i].split('review/text: ',1)[1])          
-
-
-# In[93]:
 
 #get unique words from reviews
 #L=unique_words
@@ -36,8 +27,6 @@ for i in range(len(review)):
     unique_words.update(words)
 
 
-# In[94]:
-
 #remove stopwords 
 #W=polished_words
 polished_words=unique_words.difference(stops)
@@ -46,9 +35,6 @@ W_lowerCase=[x.lower() for x in W]
 W_lower_reduced=W_lowerCase
 for word in stops:
      W_lower_reduced = [x for x in W_lower_reduced if x != word]
-
-
-# In[95]:
 
 #get top 500 words
 total_words=[]
@@ -64,27 +50,16 @@ for word in stops:
 
 #remove br
 W_tot_reduced = [x for x in W_tot_reduced if x != 'br']
-
-import collections
 freq=collections.Counter(W_tot_reduced).most_common(500)
-
-
-# In[234]:
 
 #output the 500 top words
 with open("500CommonWords.txt", "w") as output:
     output.write(str(freq))
 
-
-# In[255]:
-
 #make 500 words into features
 features=[]
 for i in range(len(freq)):
     features.append(freq[i][0])
-
-
-# In[271]:
 
 #making feature dictionary for sklearn feature extraction
 data=[]
@@ -98,23 +73,14 @@ for i in range(len(review)):
             anew[word]=a[word]
     data.append(anew)
 
-
-# In[273]:
-
 #vectorizing
 from sklearn.feature_extraction import DictVectorizer
 vec = DictVectorizer()
 vec_data=vec.fit_transform(data).toarray()
 
-
-# In[276]:
-
 #kmeans
 from sklearn.cluster import KMeans
 kmeans = KMeans(n_clusters=10, random_state=0).fit(vec_data)
-
-
-# In[299]:
 
 #saving features and centers in order
 FName=vec.get_feature_names()
@@ -129,16 +95,6 @@ result={}
 for i in range(len(sort)):
     result[i]=[(FName[x],centers[i][x]) for x in sort[i]]
                    
-
-
-# In[307]:
-
 #output the kmeans result
 with open("CentroidsTop5Words.txt", "w") as output:
     output.write(str(result))
-
-
-# In[ ]:
-
-
-
